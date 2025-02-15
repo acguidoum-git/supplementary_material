a02628dd08e180b6518a88c50e0809d3411ec9b0
acguidoum-git <acguidoum@univ-tam.dz>
2025-02-15 20:41:16 +0100

    Install_and_load_all_R_packages.R

---
+++ Install_and_load_all_R_packages.R
@@ -0,0 +1,34 @@
+if (!require("Sim.DiffProc")) {
+    install.packages("Sim.DiffProc")
+    library("Sim.DiffProc")
+}
+
+if (!require("yuima")) {
+  install.packages("yuima")
+  library("yuima")
+}
+
+if (!require("ttutils")) {
+    install.packages("ttutils")
+    library("ttutils")
+}
+
+if (!require("EQL")) {
+    install.packages("EQL")
+    library("EQL")
+}
+
+if (!require("gsl")) {
+    install.packages("gsl")
+    library("gsl")
+}
+
+if (!require("ggsci")) {
+    install.packages("ggsci")
+    library("ggsci")
+}
+
+if (!require("furrr")) {
+    install.packages("furrr")
+    library("furrr")
+}
