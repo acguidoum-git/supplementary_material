3e389cf75e7736ddcf94041d2bfe606d6988c858
A.C. GUIDOUM <acguidoum@univ-tam.dz>
2025-03-20 00:25:13 +0100

    Load_main_function.R

--- load_main_function.R
+++ load_main_function.R
@@ -12,7 +12,7 @@
 ####################
 
 Sim_mod <- function(x0,gt,k=NULL,Alpha=1,Beta=1,Gamma=1,H=0.75,
-                    T=1,N=1000,M=100,ncpu=2)
+                    T=1,N=1000,M=100,ncpu=6)
            {
    if (H <= 0) stop("'H' must be in '[0 . 1]'")
    if (H >= 1) stop("'H' must be in '[0 . 1]'")
@@ -56,28 +56,40 @@ moment <- function(x, order = 1,center = TRUE,...)
 ###      Exact moments Eq.(5): R function       ###
 ###################################################
 
-HO_mom <- function(x0,gt,k=NULL,Alpha=1,Beta=1,Gamma=1,H=0.75,p=1,t)
-        {
-    t0 <- t[which(t==0)]
-    t  <- t[which(t!=0)]
-    if (H <= 0) stop("'H' must be in '[0 . 1]'")
-    if (H >= 1) stop("'H' must be in '[0 . 1]'")
-    if (p <= 0) stop("'p' must be numeric > 0")
-    if (Gamma <= 0) stop("'Gamma' must be numeric > 0")
-    if (!is.expression(gt)) stop(" 'g(t)' must be expressions in 't'")
-    if (is.null(k)){Gt <- gt}else{
-    if (ttutils::isInteger(k)==FALSE) stop("k must be non-negative integer.")
-    Gt  <- Deriv::Deriv(gt,"t",nderiv = k)}
-    G_t <- function(t) eval(Gt)
-    A   <- function(t) 2^(-0.5*p)*((Gamma*G_t(t)^(Alpha))/1i)^p * t^(H*p)
-    B   <- function(t) 0.5*sqrt(2)*1i *((G_t(0)^(-Alpha)*x0+Beta*t )/Gamma)*t^(-H)            
-    if (ttutils::isInteger(p)){
-          Herm     <- EQL::hermite(x=B(t),n=p,prob=FALSE)}else{
-          M1 <- gsl::hyperg_1F1(a=-0.5*p,b=0.5, x=B(t)^2)
-          M2 <- gsl::hyperg_1F1(a=0.5-0.5*p,b=1.5, x=B(t)^2)
-          Herm  <- 2^(p)*sqrt(pi)*((M1/gamma(0.5-0.5*p))-2*B(t)*(M2/gamma(-0.5*p)))  
-      }
-    result <- Re(A(t)*Herm)
-    if (length(t0)==1 & t0==0) {result <- c(x0^p,result)} else { result <- result}
-    return(invisible(result))
+HO_mom <- function(x0, gt, k=NULL, Alpha=1, Beta=1, Gamma=1, H=0.75, p=1, t) {
+  if (!inherits(gt, "expression")) stop("'gt' must be an expression in 't'")
+  if (H <= 0 | H > 1) stop("'H' must be in '(0,1)'")
+  if (p <= 0) stop("'p' must be numeric > 0'")
+  if (Gamma <= 0) stop("'Gamma' must be numeric > 0'")
+  t <- as.numeric(t)
+  is_single_t <- length(t) == 1  
+  if (all(t == 0)) {
+    return(x0^p)  
+  }
+  if (is.null(k)) {
+    Gt <- gt
+  } else {
+    if (!ttutils::isInteger(k)) stop("k must be a non-negative integer.")
+    Gt <- Deriv(gt, "t", nderiv = k)
+  }
+  G_t <- function(t) sapply(t, function(ti) eval(Gt, envir = list(t = ti)))
+  G_t_0 <- G_t(0)
+  if (length(G_t_0) == 0) stop("Evaluation of g(t) at t=0 failed.")
+  A   <- function(t) ifelse(t == 0, NA, 2^(-0.5 * p) * ((Gamma * G_t(t)^Alpha) / 1i)^p * t^(H * p))
+  B   <- function(t) ifelse(t == 0, NA, 0.5 * sqrt(2) * 1i * ((G_t_0^(-Alpha) * x0 + Beta * t) / Gamma) * t^(-H))
+  compute_moment <- function(t) {
+    if (t == 0) {
+      return(x0^p)  
+    }
+    if (ttutils::isInteger(p)) {
+      Herm <- hermite(x = B(t), n = p, prob = FALSE)
+    } else {
+      M1 <- gsl::hyperg_1F1(a = -0.5 * p, b = 0.5, x = B(t)^2)
+      M2 <- gsl::hyperg_1F1(a = 0.5 - 0.5 * p, b = 1.5, x = B(t)^2)
+      Herm <- 2^p * sqrt(pi) * ((M1 / gamma(0.5 - 0.5 * p)) - 2 * B(t) * (M2 / gamma(-0.5 * p)))
+    }
+    return(Re(A(t) * Herm))
+  }
+  result <- sapply(t, compute_moment)
+  return(if (is_single_t) result else invisible(result))
 }
